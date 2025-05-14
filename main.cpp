660453069ca70ab25940f6ab58c30e796d51afa5
zxcsf4ik <shadowzxcraze1337@gmail.com>
2025-05-15 00:12:11 +0300

    Part 4: Implemented basic DFS

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
-git add main.cpp
-git commit -m "Part 3: Added empty cells counter"#include <iostream>
+#include <iostream>
 #include <vector>
 using namespace std;
 
@@ -24,4 +23,30 @@ pair<int, int> finish = {0, 6};
 
 bool isValid(int x, int y, const vector<vector<char>>& g) {
     return x >= 0 && x < SIZE && y >= 0 && y < SIZE && g[x][y] != BLOCK;
+}
+
+int countEmpty(const vector<vector<char>>& g) {
+    int count = 0;
+    for (auto& row : g)
+        for (char c : row)
+            if (c != BLOCK) count++;
+    return count;
+}
+
+bool dfs(int x, int y, vector<vector<bool>>& visited, int visitedCount, int totalEmpty) {
+    if (make_pair(x, y) == finish && visitedCount == totalEmpty)
+        return true;
+
+    vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+    for (auto [dx, dy] : directions) {
+        int nx = x + dx, ny = y + dy;
+        if (isValid(nx, ny, grid) && !visited[nx][ny]) {
+            visited[nx][ny] = true;
+            if (dfs(nx, ny, visited, visitedCount + 1, totalEmpty))
+                return true;
+            visited[nx][ny] = false;
+        }
+    }
+    return false;
 }
